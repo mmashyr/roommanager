0b49f7c44b7dd77c6391cca653c7ffc44eb334c5
mmashyr <mashyr.mark@gmail.com>
2024-06-30 14:47:52 +0200

    adds basic empty web structure

---
+++ src/main/java/com/mmashyr/roommanager/RoommanagerApplication.java
@@ -0,0 +1,13 @@
+package com.mmashyr.roommanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoommanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoommanagerApplication.class, args);
+    }
+
+}
